c57f5dc169d9fa2cffcf600dd2fd1a9f7668816d
Hari om Singh <pantrokbazz@gmail.com>
2026-02-14 22:21:16 +0530

    Clarify config comment in tailwind.config.js
    Updated the comment to clarify the purpose of the config.

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,4 +1,4 @@
-/** @type {import('tailwindcss').Config} */
+/** @type {import('tailwindcss').Config}  config all the thinng in */
 module.exports = {
   content: [
     "./src/**/*.{js,jsx,ts,tsx}",
