61420be8869ed8c02560cabe81ffec9bce3c7ad0
Hari om Singh <pantrokbazz@gmail.com>
2026-02-14 22:21:55 +0530

    Fix comment formatting in tailwind.config.js
    Correct comment in tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,4 +1,4 @@
-/** @type {import('tailwindcss').Config}  config all the thinng in */
+/** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
     "./src/**/*.{js,jsx,ts,tsx}",
